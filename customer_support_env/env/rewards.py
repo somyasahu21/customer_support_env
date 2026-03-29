697ec1de2ba3720193c004dbe04fbfbd003bb5d7
somyasahu21 <somya7101@gmail.com>
2026-03-29 15:35:55 +0530

    Update rewards.py

--- customer_support_env/env/rewards.py
+++ customer_support_env/env/rewards.py
@@ -1,27 +1,34 @@
 def compute_reward(env, action):
     reward = 0.0
 
+    # STEP EFFICIENCY
+    reward -= env.step_count * 0.03
+
+    # ACTION QUALITY
     if action.action_type == "classify":
         reward += 0.2
 
-    if action.action_type == "tool_call":
-        reward += 0.4
-
-    if action.action_type == "respond":
+    elif action.action_type == "respond":
         reward += 0.3
         env.customer_satisfaction += 0.1
 
-    if action.action_type == "resolve":
-        reward += 1.0
+    elif action.action_type == "tool_call":
+        reward += 0.4
 
-    # penalties
-    if env.step_count > 8:
+    elif action.action_type == "resolve":
+        if env.step_count <= env.current_task.get("optimal_steps", 5):
+            reward += 1.2
+        else:
+            reward += 0.6
+
+    elif action.action_type == "escalate":
+        reward += 0.3
+
+  
+    reward += env.customer_satisfaction * 0.5
+
+   
+    if len(env.history) > 2 and env.history[-1] == env.history[-2]:
         reward -= 0.2
 
-    if action.action_type not in ["classify", "respond", "tool_call", "resolve", "escalate"]:
-        reward -= 0.3
-
-    reward += env.customer_satisfaction * 0.5
-    reward -= env.step_count * 0.05
-
-    return reward
+    return max(min(reward, 2.0), -1.0)
